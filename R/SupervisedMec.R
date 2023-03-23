b6c1c1380478d9c500b1c8675fd6b1568d2e9f2c
LukaszChrostowski <lukchr@st.amu.edu.pl>
2023-03-23 17:31:23 +0100

    small changes

--- R/SupervisedMec.R
+++ R/SupervisedMec.R
@@ -5,7 +5,6 @@
 #' @param vars - a
 #' @param g - a
 #' @param blockvars - a
-#' @param string_comparator - a
 #' @param control - a
 #'
 #' @importFrom reclin2 compare_pairs
@@ -14,7 +13,7 @@
 #' @importFrom reclin2 pair
 #' @export
 #'
-mec <- function(A, B, Omega = NULL, vars, g, blockvars = NULL, string_comparator = FALSE, control = control_mec()) {
+mec <- function(A, B, Omega = NULL, vars, g, blockvars = NULL, control = control_mec()) {
 
   nA <- nrow(A)
   nB <- nrow(B)
@@ -23,11 +22,6 @@ mec <- function(A, B, Omega = NULL, vars, g, blockvars = NULL, string_comparator
     pairs <- reclin2::pair(A, B, deduplication = control$deduplication, add_xy = control$add_xy)
   } else {
     pairs <- reclin2::pair_blocking(A, B, on = blockvars, deduplication = control$deduplication, add_xy = control$add_xy)
-  }
-  if (string_comparator) {
-    reclin2::compare_pairs(pairs, on = blockvars, inplace = TRUE, default_comparator = jaro_winkler(treshold = control$comp_rate))
-  } else {
-    reclin2::compare_pairs(pairs, on = blockvars, inplace = TRUE)
   }
 
 
